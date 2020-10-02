9cedf93313ac8e5b4bf52073494cf9f9fcebb4d6
de-evg <d.e.min@yandex.ru>
2020-10-02 13:27:39 +0300

    created components for check routers

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,24 +1,49 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {BrowserRouter, Switch, Router} from "react-router-dom";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 import MainScreen from "../main-screen/main-screen";
+import AuthScreen from "../auth-screen/auth-screen";
+import MyListScreen from "../my-list-screen/my-list-screen";
+import MovieScreen from "../movie-screen/movie-screen";
+import AddReviewScreen from "../add-review-screen/add-review-screen";
+import PlayerScreen from "../player-screen/player-screen";
+
 
 const App = (props) => {
   const {promoTitle, promoGenre, promoRelease} = props;
   return (
     <BrowserRouter>
       <Switch>
-        <Router exact path="/">
+        <Route exact path="/">
           <MainScreen
             promoTitle={promoTitle}
             promoGenre={promoGenre}
             promoRelease={promoRelease}
           />
-        </Router>
+        </Route>
+
+        <Route exact path="/login">
+          <AuthScreen />
+        </Route>
+
+        <Route exact path="/myList">
+          <MyListScreen />
+        </Route>
+
+        <Route exact path="/films/:id">
+          <MovieScreen />
+        </Route>
+
+        <Route exact path="/films/:id/review">
+          <AddReviewScreen />
+        </Route>
+
+        <Route exact path="/player/:id">
+          <PlayerScreen />
+        </Route>
       </Switch>
     </BrowserRouter>
-
   );
 };
 
